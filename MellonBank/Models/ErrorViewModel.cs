36fea164309555eff01634823d09adfed3447b90
Napoleon <Napoleon@DESKTOP-F1FCGPD>
2026-04-10 12:52:25 +0300

    Add project files.

---
+++ MellonBank/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MellonBank.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
